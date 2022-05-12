b9f5128545ddba9021baddf63b52010c4e53c35d
Wobomba <iwobomba999@gmail.com>
2022-05-12 16:41:38 +0300

    Initial

---
+++ golang-crud-rest-api/main.go
@@ -0,0 +1,31 @@
+package main
+import(
+	"golang-crud-rest-api/database"
+	"golang-crud-rest-api/controllers"
+	"github.com/gorilla/mux"
+	"net/http"
+
+)
+func main(){
+	// Load Configurations from config.json using Viper
+	LoadAppConfig()
+	// Initialize Database
+	database.Connect(AppConfig.ConnectionString)
+	database.Migrate()
+	
+	// Initialize the router
+	router := mux.NewRouter().StrictSlash(true)
+	// Register Routes
+	RegisterProductRoutes(router)
+	// Start the server
+	log.Println(fmt.Sprintf("Starting Server on port %s", AppConfig.Port))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", AppConfig.Port), router))
+
+}
+func RegisterProductRoutes(router *mux.Router) {
+	router.HandleFunc("/api/products", controllers.GetProducts).Methods("GET")
+	router.HandleFunc("/api/products/{id}", controllers.GetProductById).Methods("GET")
+	router.HandleFunc("/api/products", controllers.CreateProduct).Methods("POST")
+	router.HandleFunc("/api/products/{id}", controllers.UpdateProduct).Methods("PUT")
+	router.HandleFunc("/api/products/{id}", controllers.DeleteProduct).Methods("DELETE")
+}
